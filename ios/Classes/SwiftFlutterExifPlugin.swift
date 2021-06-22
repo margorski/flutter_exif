e4b7d908acde21a73d689e87562e25d744393d96
Mariusz Górski <mariusz.gorski@axbit.com>
2021-06-22 11:34:49 +0200

    Add entry place for every interface function in iOS

--- ios/Classes/SwiftFlutterExifPlugin.swift
+++ ios/Classes/SwiftFlutterExifPlugin.swift
@@ -10,11 +10,74 @@ public class SwiftFlutterExifPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch(call.method) {
-        case "fromBytes":
-            result("iOS " + UIDevice.current.systemVersion)
-            break
-        default:
-            break;
+      case "initPath":
+        result("not implemented")
+        break
+      case "initBytes":
+        result("not implemented")
+        break
+      case "isSupportedMimeType":
+        result("not implemented")
+        break
+      case "saveAttributes":
+        result("not implemented")
+        break
+      case "getImageData":
+        result("not implemented")
+        break
+      case "setAttribute":
+        result("not implemented")
+        break
+      case "getAttribute":
+        result("not implemented")
+        break
+      case "getAttributeDouble":
+        result("not implemented")
+        break
+      case "getAttributeInt":
+        result("not implemented")
+        break
+      case "getAttributeRange":
+        result("not implemented")
+        break
+      case "flipHorizontally":
+        result("not implemented")
+        break
+      case "flipVertically":
+        result("not implemented")
+        break
+      case "getAltitude":
+        result("not implemented")
+        break
+      case "getLatLong":
+        result("not implemented")
+        break
+      case "getRotationDegrees":
+        result("not implemented")
+        break
+      case "getThumbnail":
+        result("not implemented")
+        break
+      case "hasAttribute":
+        result("not implemented")
+        break
+      case "isThumbnailCompressed":
+        result("not implemented")
+        break
+      case "isFlipped":
+        result("not implemented")
+        break
+      case "resetOrientation":
+        result("not implemented")
+        break
+      case "rotate":
+        result("not implemented")
+        break
+      case "setLatLong":
+        result("not implemented")
+        break            
+      default:
+        break;
     }
   }
 }
